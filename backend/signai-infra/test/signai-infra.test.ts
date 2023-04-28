d418b42cf8fee3d569af508f219918f18a21c599
Rastin Rashidi <rastinrashidi@gmail.com>
2023-04-27 17:42:29 -0700

    dockerfile created

---
+++ backend/signai-infra/test/signai-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SignaiInfra from '../lib/signai-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/signai-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SignaiInfra.SignaiInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
