40b7e103b9c3bac83979438ba3c07ff7dfc99e59
Rastin Rashidi <rastinrashidi@gmail.com>
2023-04-27 18:06:22 -0700

    updated infra stack

--- backend/signai-infra/lib/signai-infra-stack.ts
+++ backend/signai-infra/lib/signai-infra-stack.ts
@@ -1,38 +1,37 @@
 import { Stack, StackProps} from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
-// import * as apiGateway from 'aws-cdk-lib/aws-apigateway'
-// import * as dotenv from "dotenv"
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway'
+import * as dotenv from "dotenv"
 
-// dotenv.config()
+dotenv.config()
 
 export class SignaiInfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // const layer = new lambda.LayerVersion(this, "BaseLayer", {
-    //   code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
-    //   compatibleRuntimes: [lambda.Runtime.PYTHON_3_9]
-    // })
+    const layer = new lambda.LayerVersion(this, "BaseLayer", {
+      code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_9]
+    })
 
     const apiLambda = new lambda.Function(this, "ApiFunction", {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset("../app/"),
       handler: "signai_api.handler",
-      // layers: [layer],
-      // environment: {
-      //   OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? "",
+      layers: [layer],
+      environment: {
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? "",
       }
-    // }
-    )
+    })
 
-    // const signaiApi = new apiGateway.RestApi(this, "RestApi", {
-    //   restApiName: "Signai API"
-    // })
+    const signaiApi = new apiGateway.RestApi(this, "RestApi", {
+      restApiName: "Signai API"
+    })
 
-    // const lambdaApiIntegration = new apiGateway.LambdaIntegration(apiLambda)
-    // signaiApi.root.addProxy({
-    //   defaultIntegration: lambdaApiIntegration
-    // })
+    const lambdaApiIntegration = new apiGateway.LambdaIntegration(apiLambda)
+    signaiApi.root.addProxy({
+      defaultIntegration: lambdaApiIntegration
+    })
   }
 }
